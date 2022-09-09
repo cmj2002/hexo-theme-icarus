57bf7188d75acb4bea3ca53fa6345a72eb07158c
Cao Mingjun <me@caomingjun.com>
2022-09-09 16:26:22 +0800

    Customize

--- layout/common/head.jsx
+++ layout/common/head.jsx
@@ -166,7 +166,7 @@ module.exports = class extends Component {
             {rss ? <link rel="alternate" href={url_for(rss)} title={config.title} type="application/atom+xml" /> : null}
             {favicon ? <link rel="icon" href={url_for(favicon)} /> : null}
             <link rel="stylesheet" href={iconcdn()} />
-            {hlTheme ? <link rel="stylesheet" href={cdn('highlight.js', '9.12.0', 'styles/' + hlTheme + '.css')} /> : null}
+            {hlTheme ? <link rel="stylesheet" href={cdn('highlight.js', '11.6.0', 'styles/' + hlTheme + '.css')} /> : null}
             <link rel="stylesheet" href={fontCssUrl[variant]} />
             <link rel="stylesheet" href={url_for('/css/' + variant + '.css')} />
             <Plugins site={site} config={config} helper={helper} page={page} head={true} />
@@ -175,6 +175,7 @@ module.exports = class extends Component {
                 src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" async></script> : null}
 
             {followItVerificationCode ? <meta name="follow.it-verification-code" content={followItVerificationCode} /> : null}
+            <script defer data-domain="blog.caomingjun.com" src="https://analytics.caomingjun.com/js/plausible.js"></script>
         </head>;
     }
 };
